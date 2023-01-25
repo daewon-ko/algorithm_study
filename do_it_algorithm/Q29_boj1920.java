7fe08e1f41656f2e321e8f08ea3c2681a2ecc479
daewon-ko <eodnjs568@naver.com>
2023-01-26 01:13:26 +0900

    프로그래머스 k번쨰 수 구현

--- do_it_algorithm/Q29_boj1920.java
+++ do_it_algorithm/Q29_boj1920.java
@@ -1,52 +1,52 @@
 package do_it_algorithm;
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Q29_boj1920 {
-    public static void main(String[] args) throws IOException{
+
+    public static void main(String[] args) throws  IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int [] A = new int[n];
+        int [] data = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i =0; i<n; i++){
-            A[i] = Integer.parseInt(st.nextToken());
+        for(int i= 0; i<n; i++){
+            data[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(A);
-
+        Arrays.sort(data);
+        // 찾아야하는 숫자의 개수
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i<m; i++){
-            int wanted = Integer.parseInt(st.nextToken());
+        for(int i =0; i<m; i++){
             boolean find = false;
-            // 인덱스 번호 스타트 0
+            int wanted = Integer.parseInt(st.nextToken());
             int start = 0;
-            // 인덱스 번호 마지막 배열 크기 -1
-            int end = A.length-1;
-
+            int end = data.length-1;
             while (start<=end){
-                int mid = (start+end)/2;
-                int mid_value = A[mid];
-                if(wanted<mid_value){
-                    end = mid -1;
+                int midIndex = (start+end)/2;
+                // mid의 인덱스의 실제 value값
+                int midValue = data[midIndex];
+                if(midValue>wanted)
+                    end= midIndex-1;
+                else if(midValue<wanted)
+                    start= midIndex+1;
+                else{
+                    find =true;
+                    break;
                 }
-                else if(wanted>mid_value){
-                    start = mid+1;
-                }
-                find = true;
-                break;
-            }
 
-            if(find){
-                System.out.println(1);
-            } else{
-                System.out.println(0);
             }
-
+            if(find) System.out.println(1);
+            else System.out.println(0);
         }
 
+
+
+
+
     }
+
 }
+
--- programmers/highScoreKit/Kth_Number.java
+++ programmers/highScoreKit/Kth_Number.java
@@ -1,32 +1,33 @@
+package programmers.highScoreKit;
 import java.util.*;
 import java.io.*;
+// 고득점 키트 - 정렬 - k번쨰 수
+public class Kth_Number {
 
-public class Main {
-
-    public static void main(String[] args) throws IOException {
-
-    }
-
-
-
-}
-class Solution {
     class Solution {
         public int[] solution(int[] array, int[][] commands) {
-            int[] answer = {};
-            int [] converter = {};
+            int[] answer = new int[commands.length];
             int i,j,k = 0;
 
             for(int m = 0; m<commands.length; m++){
+
                 int num = 0;
                 i = commands[m][0]-1;
                 j = commands[m][1]-1;
                 k = commands[m][2]-1;
+                int len = j-i +1;
+                int [] converter = new int[len];
 
 
-                converter=Arrays.copyOfRange(array,i,j);
+                // for(int z= 0; z<len; z++){
+                //     converter[z] = array[i+z];
+                // }
+
+//                converter=Arrays.copyOfRange(array[m],i,j);
+
 
                 Arrays.sort(converter);
+                System.out.println(k);
                 num = converter[k];
                 answer[m] = num;
 
@@ -36,4 +37,5 @@ class Solution {
         }
 
     }
+
 }
