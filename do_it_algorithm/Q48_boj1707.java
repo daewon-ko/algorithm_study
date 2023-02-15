9da8a6fed19d31a3edc1d23d5a6b9557e5ac0c27
daewon-ko <eodnjs568@naver.com>
2023-02-15 16:09:34 +0900

    48번 문제 구현

---
+++ do_it_algorithm/Q48_boj1707.java
@@ -0,0 +1,72 @@
+package do_it_algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+// 백준 이분그래프 판별하기 문제
+public class Q48_boj1707 {
+    private static List<Integer> gragh[];
+    private static int[] colors;
+    private static final int RED = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(br.readLine());
+        for (int i = 0; i < k; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int v = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            gragh = new ArrayList[v + 1];
+            colors = new int[v + 1];
+
+            for (int j = 1; j <= v; j++) {
+                gragh[j] = new ArrayList<>();
+            }
+
+            for (int p = 0; p < e; p++) {
+                st = new StringTokenizer(br.readLine());
+                int start = Integer.parseInt(st.nextToken());
+                int end = Integer.parseInt(st.nextToken());
+                gragh[start].add(end);
+                gragh[end].add(start);
+            }
+
+            boolean isDividedGraph = false;
+            for (int j = 1; j <= v; j++) {
+                if (colors[j] == 0) {
+                    isDividedGraph = BFS(j, RED);
+                }
+                if (!isDividedGraph) break;
+            }
+            if (isDividedGraph) System.out.println("YES");
+            else System.out.println("NO");
+        }
+        br.close();
+
+
+    }
+
+    private static boolean BFS(int node, int color) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(node);
+        colors[node] = color;
+        while (!queue.isEmpty()) {
+            int now_node = queue.poll();
+            for (int next : gragh[now_node]) {
+                if (colors[now_node] == colors[next]) {
+                    return false;
+                }
+
+                if (colors[next] == 0) {
+                    colors[next] = colors[now_node] * -1;
+                    queue.offer(next);
+
+                }
+            }
+        }
+        return true;
+    }
+}
+
