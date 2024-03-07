0f9e507e5cb5d57defbf2b4418593bb0eb3147f8
pinetree <eodnjs568@gmail.com>
2024-03-07 19:47:08 +0900

    없는숫자 더하기

---
+++ programmers/programmers/src/main/java/level1/없는숫자더하기.java
@@ -0,0 +1,15 @@
+package level1;
+import java.util.Arrays;
+
+public class 없는숫자더하기 {
+    public int solution(int[] numbers) {
+
+        int answer = 0;
+
+        return 45-Arrays.stream(numbers).sum();
+
+
+
+
+    }
+}
