244cb403a4d8ce1c8e6b7ea8aaa90b8a816084bc
pinetree <eodnjs568@gmail.com>
2024-05-08 23:12:16 +0900

    백준 토마토 7576 구현
    - dx,dy 오류

--- boj/boj7576.java
+++ boj/boj7576.java
@@ -37,7 +37,7 @@ public class boj7576 {
     static int[][] graph;
     static boolean[][] visited;
     static Queue<Tomato> queue = new LinkedList<>();
-    static int[] dx = {1, 0, -1, 1};
+    static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, -1, 0, 1};
 
     static class Tomato {
