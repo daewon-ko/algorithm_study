ffe23e8e1e34eaca9da6baef709386fe65c08daa
pinetree <eodnjs568@gmail.com>
2024-06-21 20:45:54 +0900

    백준 13549 숨바꼭질3

---
+++ boj/barkingdog/bfs_dfs/boj13549.java
@@ -0,0 +1,52 @@
+
+package boj.barkingdog.bfs_dfs;
+
+import java.io.*;
+import java.util.*;
+
+// 백준 13549
+public class boj13549 {
+    static int min = Integer.MAX_VALUE;
+    static int n, k;
+    static boolean[] visited;
+    static int max = 10000 + 10;
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        n = scan.nextInt();
+        k = scan.nextInt();
+
+        visited = new boolean[max];
+        bfs();
+        System.out.println(min);
+
+    }
+
+    public static void bfs() {
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(n, 0));
+
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+            visited[node.x] = true;
+            if (node.x == k) min = Math.min(min, node.time);
+
+            if (node.x * 2 <= max && visited[node.x * 2] == false) q.offer(new Node(node.x * 2, node.time));
+            if (node.x + 1 <= max && visited[node.x + 1] == false) q.offer(new Node(node.x + 1, node.time + 1));
+            if (node.x - 1 >= 0 && visited[node.x - 1] == false) q.offer(new Node(node.x - 1, node.time + 1));
+        }
+    }
+
+    public static class Node {
+        int x;
+        int time;
+
+        public Node(int x, int time) {
+            this.x = x;
+            this.time = time;
+        }
+    }
+
+
+}
