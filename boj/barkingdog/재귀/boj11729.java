dbe0da28eb7a2c1fc428966e7d25360b3357cdae
pinetree <eodnjs568@gmail.com>
2024-06-25 19:57:39 +0900

    백준 11729
    - 하노이의 탑
    - base condition, n이 만족할때 -> n+1이 만족하면 재귀가 성립한다는 것이 잘 떠오르지 않음

---
+++ boj/barkingdog/재귀/boj11729.java
@@ -0,0 +1,31 @@
+package boj.barkingdog.재귀;
+
+import java.util.Scanner;
+
+/**
+ * 백준 하노이의탑 순서
+ */
+public class boj11729 {
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println((1 << n) - 1);
+        func(1, 3, n);
+        System.out.println(sb.toString());
+        sc.close();
+
+
+
+    }
+
+    static void func(int a, int b, int n) {
+        if (n == 1) {
+            sb.append(a).append(" ").append(b).append("\n");
+            return;
+        }
+        func(a, 6 - a - b, n - 1);
+        sb.append(a).append(" ").append(b).append("\n");
+        func(6 - a - b, b, n - 1);
+    }
+}
