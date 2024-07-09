3e5982aaf5467140e3768558447f865b487e429d
pinetree <eodnjs568@gmail.com>
2024-07-09 22:09:44 +0900

    백준 15656 n과 m(7)

---
+++ boj/backtracking/boj15656.java
@@ -0,0 +1,61 @@
+package boj.backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class boj15656 {
+
+
+    /**
+     * n과 m(6)
+     */
+    static int n, m;
+    static int[] arr;
+    static final int MAX = 8 + 1;
+    static boolean[] visited;
+    static List<Integer> list = new ArrayList<>();
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[MAX];
+        visited = new boolean[MAX];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(list);
+
+        func( 0);
+        System.out.println(sb.toString());
+
+    }
+
+    public static void func( int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+                arr[depth] = list.get(i);
+                func(depth+1);
+        }
+    }
+}
+
