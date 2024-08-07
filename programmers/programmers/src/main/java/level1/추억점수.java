2d0dbff3fa54a6142055bc2788eb4eafa54b4cd3
pinetree <eodnjs568@gmail.com>
2024-08-07 22:17:23 +0900

    프로그래머스 추억점수
    - Map 이용하거나
    - Stream 이용하거나 또는 반복문

--- programmers/programmers/src/main/java/level1/추억점수.java
+++ programmers/programmers/src/main/java/level1/추억점수.java
@@ -1,4 +1,4 @@
-package level1;
+package programmers.programmers.src.main.java.level1;
 
 import java.util.*;
 import java.util.stream.IntStream;
@@ -12,45 +12,23 @@ public class 추억점수 {
             nameBindingYear.put(name[i], yearning[i]);
         });
 
-        for (String[] peopleList : photo) {
 
-//            IntStream.range(0, peopleList.length).filter(
-//
-//            );
-
-            for (String people : peopleList) {
-
-                if (nameBindingYear.containsKey(people)) {
-//                    scoreList[] += nameBindingYear.get(people);
-                }
-            }
-        }
 
         for (int i = 0; i < photo.length; i++) {
             String[] peopleList = photo[i];
             for (int j = 0; j < peopleList[j].length(); j++) {
                 String s = peopleList[j];
-                IntStream.range(0, name.length).filter(
-                        k -> s.equals(name[k])
-                ).forEach(k -> scoreList[k]+=yearning[k]);
+                if (nameBindingYear.containsKey(s)) {
+                    scoreList[i]+=nameBindingYear.get(s);
+                }
+
+//                IntStream.range(0, name.length).filter(
+//                        k -> s.equals(name[k])
+//                ).forEach(k -> scoreList[k]+=yearning[k]);
             }
         }
 
-//        for (String[] peopleList : photo) {
-//            IntStream.range(0, photo.length).filter(
-//                    i -> peopleList[i].equals()
-//            )
-//
-//        }
 
-//        Map<String[], Integer> photoScore = createPhotoMap(photo);
-//
-//        for (Map.Entry<String[], Integer> entry : photoScore.entrySet()) {
-//            String[] peopleList = entry.getKey();
-//            IntStream.range(0, Math.min(peopleList.length, name.length)).filter(
-//                    i -> peopleList[i].equals(name[i])
-//            ).forEach(i -> scoreList[i] += yearning[i]);
-//        }
         return scoreList;
     }
 
