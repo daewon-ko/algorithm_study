4947e341de7be9cd0a85106800b78422e0a5b7af
pinetree <eodnjs568@gmail.com>
2024-09-21 20:26:03 +0900

    백준 10972 실패
    - 순열을 순서대로 오름차순으로 푸는 것은 성공했으나, 어딘가 잘 생각이 나지 않음

---
+++ boj/bruteforce/boj10972.java
@@ -0,0 +1,70 @@
+package boj.bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 다음순열
+ */
+public class boj10972 {
+    static int n;
+    static boolean [] visited;
+    static List<Integer> before = new ArrayList<>();
+    static List<Integer> after = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        visited = new boolean[n+1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            before.add(Integer.parseInt(st.nextToken()));
+        }
+
+        func(0, 1);
+
+
+
+    }
+
+    public static void func(int depth, int start) {
+        if (depth == n) {
+
+            boolean flag = true;
+//
+//            for (int i = 0; i < before.size(); i++) {
+//
+//                if(!before.get(i).equals(after.get(i))){
+//                    flag = false;
+//                }
+//            }
+
+            if (flag) {
+                for (int i = 0; i < n; i++) {
+                    System.out.print(after.get(i)+" ");
+                }
+            }
+            System.out.println();
+
+            return;
+
+        }
+
+
+        for (int i = start; i <=n; i++) {
+            if(!visited[i]){
+                visited[i] = true;
+                after.add(i);
+                func(depth+1, start);
+                after.remove(after.size()-1);
+                visited[i] = false;
+
+            }
+        }
+    }
+}
