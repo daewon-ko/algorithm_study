6654b41d82fb47495f457ac3a0e3f65d8e069b6c
pinetree <eodnjs568@gmail.com>
2024-09-22 20:42:22 +0900

    백준 18290 Re
    - 대각선으로 접근하는 방향이 잘못되었음.

--- boj/bruteforce/boj18290.java
+++ boj/bruteforce/boj18290.java
@@ -10,11 +10,11 @@ import java.util.StringTokenizer;
  */
 public class boj18290 {
     static int n, m, k;
-//    static int[] dx = {1, 1, -1, -1};
-//    static int[] dy = {1, -1, -1, 1};
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
 
-    static int[] dy = {1, 1, -1, -1};
-    static int[] dx = {1, -1, -1, 1};
+//    static int[] dy = {1, 1, -1, -1};
+//    static int[] dx = {1, -1, -1, 1};
     static int[][] arr;
     static boolean[][] visited;
 
@@ -64,14 +64,15 @@ public class boj18290 {
             int newX = x + dx[i];
             int newY = y + dy[i];
 
-            if (inRange(newY, newX) && !visited[newY][newX]) {
+            if (inRange(newY, newX) && visited[newY][newX]) {
                 dfs(newY, newX, sum +arr[newY][newX], cnt + 1);
-                visited[newY][newX] = false;
+//                visited[newY][newX] = false;
             }
 
 
         }
 
+        visited[y][x] = false;
 
     }
 
@@ -79,5 +80,73 @@ public class boj18290 {
         return y >= 0 && y < n && x >= 0 && x < m;
     }
 
+
+}
+
+class boj18290_re{
+
+    public class Main {
+        static int n, m, k;
+        static int[][] arr;
+        static boolean[][] visited;
+
+        static int MAX = Integer.MIN_VALUE;
+        static int[] dx = {0, 0, 1, -1}; // 상하좌우
+        static int[] dy = {1, -1, 0, 0}; // 상하좌우
+
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            k = Integer.parseInt(st.nextToken());
+
+            arr = new int[n][m];
+            visited = new boolean[n][m];
+
+            for (int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < m; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            dfs(0, 0, 0, 0);
+
+            System.out.println(MAX);
+        }
+
+        public static void dfs(int y, int x, int sum, int cnt) {
+            if (cnt == k) {
+                MAX = Math.max(sum, MAX);
+                return;
+            }
+
+            for (int i = y; i < n; i++) {
+                for (int j = (i == y ? x : 0); j < m; j++) {
+                    if (canSelect(i, j)) {
+                        visited[i][j] = true;
+                        dfs(i, j, sum + arr[i][j], cnt + 1);
+                        visited[i][j] = false;
+                    }
+                }
+            }
+        }
+
+        public static boolean canSelect(int y, int x) {
+            if (visited[y][x]) return false;
+            for (int i = 0; i < 4; i++) {
+                int newY = y + dy[i];
+                int newX = x + dx[i];
+                if (inRange(newY, newX) && visited[newY][newX]) return false;
+            }
+            return true;
+        }
+
+        public static boolean inRange(int y, int x) {
+            return y >= 0 && y < n && x >= 0 && x < m;
+        }
+    }
 
 }
