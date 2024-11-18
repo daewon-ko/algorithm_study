12ba032ece4be65ff8e91458769f65065651dca7
pinetree <eodnjs568@gmail.com>
2024-11-19 06:38:33 +0900

    프로그래머스 미로탈출
    - 참고풀이
    - BFS 활용

--- programmers/programmers/src/main/java/level2/미로탈출.java
+++ programmers/programmers/src/main/java/level2/미로탈출.java
@@ -1,5 +1,8 @@
 package programmers.programmers.src.main.java.level2;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class 미로탈출 {
 
     static String[][] graph;
@@ -90,4 +93,119 @@ public class 미로탈출 {
         return y >= 0 && y < n && x >= 0 && x < 5;
 
     }
+}
+
+
+class 미로탈출Ref{
+    static int n;
+    static String [][] graph;
+    static boolean[][] visited;
+
+    static int startX, startY, leverY, leverX, endY, endX;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, -1, 0, 1};
+
+    public int solution(String[] maps) {
+
+        int answer = 0;
+
+        n = maps.length;
+        graph = new String[n][5];
+        visited = new boolean[n][5];
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 5; j++) {
+                graph[i][j] = String.valueOf(maps[i].charAt(j));
+
+                if(graph[i][j].equals("S")) {
+                    startY = i;
+                    startX = j;
+                } else if (graph[i][j].equals("L")) {
+                    leverY = i;
+                    leverX = j;
+                }
+                else if(graph[i][j].equals("E")) {
+                    endY = i;
+                    endX = j;
+
+                }
+            }
+        }
+
+
+        int leverDistance = bfs(startY, startX, leverY, leverX);
+        if (leverDistance == -1) {
+            return -1;
+        }
+        int leverToEndDistance = bfs(leverY, leverX, startY, startX);
+        if (leverToEndDistance == -1) {
+            return -1;
+        }
+
+        answer = leverDistance + leverToEndDistance;
+
+        return answer;
+
+
+    }
+
+    public static int bfs(int startY, int startX, int targetY, int targetX) {
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(new Node(startY, startX, 0));
+        visited[startY][startX] = true;
+
+        while (!queue.isEmpty()) {
+            Node poll = queue.poll();
+            int pollY = poll.y;
+            int pollX = poll.x;
+            int pollDistance = poll.distance;
+
+            // 종료조건
+            if (pollY == targetY && pollX == targetX) {
+                return pollDistance;
+            }
+
+
+            for (int i = 0; i < 4; i++) {
+                int newY = pollY + dy[i];
+                int newX = pollX + dx[i];
+                if (inRange(newY, newX) && !visited[newY][newX] && !graph[newY][newX].equals("X")) {
+                    visited[newY][newX] = true;
+                    queue.offer(new Node(newY, newX, pollDistance+1));
+                }
+
+
+            }
+
+        }
+
+
+        // 목표지점 도달 불가능 시
+        return -1;
+
+
+
+
+    }
+
+    public static boolean inRange(int y, int x) {
+        return y >= 0 && y < n && x >= 0 && x < 5;
+
+    }
+
+    static class Node{
+        int y;
+        int x;
+        int distance;
+
+        Node(int y, int x, int distance){
+            this.y= y;
+            this.x = x;
+            this.distance = distance;
+        }
+    }
+
+
 }
