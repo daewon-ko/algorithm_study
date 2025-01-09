df76812e244658a64fdf4120daaa8e20a1489eaf
pinetree <eodnjs568@gmail.com>
2025-01-09 10:13:38 +0900

    백준 1874 스택수열
    - 스택이용

--- boj/스터디/boj1874.java
+++ boj/스터디/boj1874.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class boj1874 {
     static int n;
@@ -57,4 +58,42 @@ public class boj1874 {
     }
 }
 
+
+class boj1874Ref{
+    static int n;
+    static List<String> list = new ArrayList<>();
+    static int current;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        current = 1;
+
+        for(int i=0; i<n; i++){
+            int target = Integer.parseInt(br.readLine());
+
+            while (current <= target) {
+                stack.push(current);
+                list.add("+");
+                current++;
+            }
+
+            if (!stack.isEmpty() && stack.peek() == target) {
+                stack.pop();
+                list.add("-");
+
+            }else {
+                System.out.println("NO");
+                return;
+            }
+
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+    }
 }
+
