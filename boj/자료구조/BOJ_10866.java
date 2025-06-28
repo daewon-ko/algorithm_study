9e8e0b5efecae1d4122312fbc4b1a7eba2c2320f
pinetree <eodnjs568@gmail.com>
2025-06-28 09:54:33 +0900

    feat : BOJ / 10866 / 실버4

---
+++ boj/자료구조/BOJ_10866.java
@@ -0,0 +1,50 @@
+package boj.자료구조;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class BOJ_10866 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Deque<Integer> deque = new LinkedList<>();
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            String cmd = input[0];
+
+            switch (cmd) {
+                case "push_front":
+                    deque.addFirst(Integer.parseInt(input[1]));
+                    break;
+                case "push_back":
+                    deque.addLast(Integer.parseInt(input[1]));
+                    break;
+                case "pop_front":
+                    sb.append(deque.isEmpty() ? -1 : deque.pollFirst()).append("\n");
+                    break;
+                case "pop_back":
+                    sb.append(deque.isEmpty() ? -1 : deque.pollLast()).append("\n");
+                    break;
+                case "size":
+                    sb.append(deque.size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(deque.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case "front":
+                    sb.append(deque.isEmpty() ? -1 : deque.peekFirst()).append("\n");
+                    break;
+                case "back":
+                    sb.append(deque.isEmpty() ? -1 : deque.peekLast()).append("\n");
+                    break;
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
